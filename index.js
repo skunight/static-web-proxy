f34348a8b4dbe1ec975afbc94f4866f26da8bed1
zzy <skunight@gmail.com>
2018-07-12 12:36:10 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const log4js = require('log4js')
 const history = require('connect-history-api-fallback')
 const httpproxy = require('./libs/proxy')
 class Proxy {
@@ -12,12 +13,22 @@ class Proxy {
   }
 
   start() {
+    log4js.configure({
+      appenders: { console: { type: 'console' } },
+      categories: { default: { appenders: [ 'console' ], level: 'info' } }
+    })
+    const logger = log4js.getLogger()
+    this.app.use(log4js.connectLogger(logger));
     this.app.use(this.proxy.path ,httpproxy({
       host: this.proxy.host,
       port: this.proxy.port,
       path: this.proxy.path,
       auth: this.proxy.auth
     }))
+    this.app.use(function (req, res, next) {
+      res.set('Cache-Control', 'no-cache')
+      next()
+    })
     this.app.use(history())
     this.app.use(express.static(this.web.dir))
     this.app.listen(this.bind.port,this.bind.host)
