0afb510bfe2dfef56973118c9241f269190e3a89
zzy <zzy@bigcruise.cn>
2023-05-22 11:50:56 +0800

    update

--- libs/proxy.js
+++ libs/proxy.js
@@ -1,6 +1,6 @@
 const httpProxy = require('http-proxy-middleware');
 const _ = require('lodash')
-
+const Sentry = require('@sentry/node')
 
 module.exports = (opts) => {
   const options = {
@@ -8,13 +8,20 @@ module.exports = (opts) => {
     changeOrigin: true,
     ws: true,
     pathRewrite: {
-      [opts.path] : '/' 
+      [opts.path] : '/'
     },
-    onProxyReq: (proxyReq, req, res, options) => {
-      if (opts.auth && _.isFunction(opts.auth)) {
-        opts.auth(proxyReq, res)
-      }
-    },
+    on: {
+      proxyReq: (proxyReq, req, res) => {
+        if (opts.auth && _.isFunction(opts.auth)) {
+          opts.auth(proxyReq, res)
+        }
+      },
+      error: (err, req, res) => {
+        Sentry.captureException(err)
+        console.error(err)
+        res.end()
+      },
+    }
   }
   return httpProxy(options)
 }
